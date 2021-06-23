cdf1d7fbd1595e11db7eaae6478cf32156fae968
Timo Hausmann <hello.timohausmann@gmail.com>
2021-06-23 19:52:26 +0200

    skills sum

--- src/sheets/coriolis/Field.svelte
+++ src/sheets/coriolis/Field.svelte
@@ -5,15 +5,32 @@
     export let label;
     export let max = 99;
     export let name = '';
-    
+    export let base = ''; //Coriolis Base Attr for Skills
+    export let skillType = ''; //Coriolis "common" or "special"
+
     let currField_value = ''
     let readonly = null;
+    let meta = '';
+    let metaTitle = '';
 
     const unsubscribe = currCharStore.subscribe(value => {
 
         if(!value) return;
         readonly = value.readonly ? true : null;
         currField_value = value[name] ? parseInt(value[name]) : ''
+
+        if(base) {
+            const baseField_value = parseInt(value[base]);
+
+            //show sum for common skill or skilled special skill
+            if(skillType === 'common' || (baseField_value > 0 && currField_value > 0)) {
+                meta = '∑ ' + (currField_value + value[base]) || '0';
+
+                //uggh yeah it's bad but it works
+                const parts = label.split(' (');
+                metaTitle = `${currField_value || 0} ${parts[0]} + ${value[base] || 0} ${parts[1].replace(')', '')}`;
+            }
+        }
     })
 
     function onChange(e) {
@@ -49,10 +66,18 @@
         border-radius: 0.25rem;
         border: none;
     }
+    small {
+        font-size: 0.75rem;
+        opacity: 0.8;
+    }
 </style>
 
 <div class="sheetField">
-    <label for={name}>{label}</label>
+    <label for={name} title={metaTitle}>{label} 
+        {#if meta}
+            <small>{meta}</small>
+        {/if}
+    </label>
     <input id={name} type="number" name={name} min="0" max={max} value={currField_value}
         readonly={readonly} on:change={onChange} on:keydown={onChange} />
 </div>
